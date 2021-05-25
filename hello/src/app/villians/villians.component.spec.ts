198208116dddb6834c6a0952599508e1bd7f49d4
Joel Ferguson <jferguson@BEL-MBP-C02F6573MD6R.local>
2021-05-25 10:53:08 +0100

    Some new folders from John Papa video

---
+++ hello/src/app/villians/villians.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VilliansComponent } from './villians.component';
+
+describe('VilliansComponent', () => {
+  let component: VilliansComponent;
+  let fixture: ComponentFixture<VilliansComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VilliansComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VilliansComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ hello/src/app/villians/villians.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-villians',
+  template: `
+    <p>
+      villians works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class VilliansComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
